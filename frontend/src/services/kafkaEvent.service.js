7c62e03a21872400249e3a78c4f5eff7c2f9b51d
Joao Gabriel Rocha <jgabriel.b.rocha@edu.ufes.br>
2026-06-11 14:54:49 -0300

    fix: Diagram events were sent to the API instead of the websocket

--- frontend/src/services/kafkaEvent.service.js
+++ frontend/src/services/kafkaEvent.service.js
@@ -2,5 +2,5 @@ import { webSocketService } from '@/services/websocket.service';
 
 export const publishDiagramEvent = (diagramId, action, data) => {
     webSocketService.emitDiagramEvent(diagramId, action, data);
-    return Promise.resolve({ status: "broadcast_sent_via_ws" });
+    return Promise.resolve({ status: "broadcast_sent" });
 };
